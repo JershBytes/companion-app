50f0a9be901cb32aa486319dc46ab0d81a4b8166
Joshua Ross <josh@rossjm.dev>
2024-10-02 22:35:45 -0400

    fixed text box issue, added js to do pushover notifications

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
 function saveData() {
-    const formData = {
-      fullname: document.getElementById('fullname').value,
-      age: document.getElementById('age').value,
-      hobbies: document.getElementById('hobbies').value,
-      favfood: document.getElementById('favfood').value,
-      question1: document.getElementById('question1').value,
-      question2: document.getElementById('question2').value,
-      question3: document.getElementById('question3').value,
-      question4: document.getElementById('question4').value,
-      question5: document.getElementById('question5').value,
-      question6: document.getElementById('question6').value,
-      hrq1: document.getElementById('hrq1').value,
-      hrq2: document.getElementById('hrq2').value,
-      hrq3: document.getElementById('hrq3').value,
-      hrq4: document.getElementById('hrq4').value,
-      hrq5: document.getElementById('hrq5').value,
-      hrq6: document.getElementById('hrq6').value,
-      hrq7: document.getElementById('hrq7').value,
-      hrq8: document.getElementById('hrq8').value,
-      hrq9: document.getElementById('hrq9').value
-    };
-  
-    const fileData = `
-      Full Name: ${formData.fullname}
-      Birthday: ${formData.age}
-      Hobbies: ${formData.hobbies}
-      Favorite Food: ${formData.favfood}
-  
-      What do you like about me?: ${formData.question1}
-      Perfect Saturday: ${formData.question2}
-      Cheer-up Strategy: ${formData.question3}
-      Vibe Description: ${formData.question4}
-      Ideal First Date: ${formData.question5}
-      Movie Genre: ${formData.question6}
-  
-      HR Questions:
-      Crime Conviction: ${formData.hrq1}
-      Drug Use: ${formData.hrq2}
-      Current Hoes: ${formData.hrq3}
-      Situationships: ${formData.hrq4}
-      Kids' Activities: ${formData.hrq5}
-      Employment Status: ${formData.hrq7}
-      Family Relationship: ${formData.hrq8}
-      Relationship Type: ${formData.hrq9}
-    `;
-  
-    const blob = new Blob([fileData], { type: "text/plain;charset=utf-8" });
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = `${formData.fullname}_application.txt`;
-    link.click();
-  }
-  
+  const formFields = [
+    'fullname', 'age', 'hobbies', 'favfood', 
+    'question1', 'question2', 'question3', 
+    'question4', 'question5', 'question6',
+    'hrq1', 'hrq2', 'hrq3', 'hrq4', 'hrq5', 
+    'hrq6', 'hrq7', 'hrq8', 'hrq9'
+  ];
+
+  const formData = {};
+  formFields.forEach(field => formData[field] = document.getElementById(field).value);
+
+  const messageBody = `
+    New application received from ${formData.fullname}!
+
+    ### Basic Info ###
+    Full Name: ${formData.fullname}
+    Birthday: ${formData.age}
+    Hobbies: ${formData.hobbies}
+    Favorite Food: ${formData.favfood}
+
+    ### Questionnaire ###
+    What do you like about me?: ${formData.question1}
+    Perfect Saturday: ${formData.question2}
+    Cheer-up Strategy: ${formData.question3}
+    Vibe Description: ${formData.question4}
+    Ideal First Date: ${formData.question5}
+    Movie Genre: ${formData.question6}
+    
+    ### HR Questionnaire ###
+    HR Questions:
+    Crime Conviction: ${formData.hrq1}
+    Drug Use: ${formData.hrq2}
+    Current Relationships: ${formData.hrq3}
+    Situationships: ${formData.hrq4}
+    Kids' Activities: ${formData.hrq5}
+    Employment Status: ${formData.hrq6}
+    Family Relationship: ${formData.hrq7}
+    Relationship Type: ${formData.hrq8}
+    Additional Comments: ${formData.hrq9}
+  `;
+
+  // Send to Pushover
+  const userKey = 'u8axcces39tpq5dw6tdp89xpwx3hsq';
+  const apiToken = 'ajtk5dvhi2jifkvpbrpp596f95ykdh';
+
+  const bodyFormDataPushover = new URLSearchParams();
+  bodyFormDataPushover.append('token', apiToken);
+  bodyFormDataPushover.append('user', userKey);
+  bodyFormDataPushover.append('message', messageBody);
+
+  const urlPushover = "https://api.pushover.net/1/messages.json";
+
+  axios({
+    method: "post",
+    url: urlPushover,
+    data: bodyFormDataPushover.toString(),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded" // Pushover needs this content type
+    }
+  })
+  .then((response) => console.log("Pushover notification sent successfully!", response.data))
+  .catch((error) => {
+    if (error.response) {
+      console.error("Pushover Error response:", error.response.data);
+    } else if (error.request) {
+      console.error("Pushover Error request:", error.request);
+    } else {
+      console.error("Pushover General error:", error.message);
+    }
+  });
+}
