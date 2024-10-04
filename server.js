69964ed5962a90ee84213634e20466d738fd0d83
Joshua Ross <josh@rossjm.dev>
2024-10-04 00:27:39 -0400

    added favicon and tested nodejs server. Will test email and push tomorrow

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('require');
-const app = express()
+const express = require('express');
+const app = express();
 
 const PORT = process.env.port || 5000;
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + '/public/index.html')
+})
 
 // Middleware
 app.use(express.static('public'));
 
 
 
-
 app.listen(PORT, ()=>{
   console.log(`Server is running on port ${PORT}`)
 })
+
 
