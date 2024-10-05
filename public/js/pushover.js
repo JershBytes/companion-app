a51008379b8d580772d0cb48a1f0adc45e9a03bb
Joshua Ross <josh@rossjm.dev>
2024-10-05 01:14:02 -0400

    Update pushover.js

--- public/js/pushover.js
+++ public/js/pushover.js
@@ -24,31 +24,32 @@ export const notifyFromForm = (formData) => {
 
   // Create a formatted message
   const message = `
-    New Submission from ${fullname} at ${email}:
+    New Submission from *${fullname}* at ${email}:
     *Age*: ${age}
     *Email*: ${email}
     *Hobbies*: ${hobbies}
     *Favorite Food*: ${favfood}
 
     *Responses:*
-    - What do you like about me? ${questions.question1}
-    - What is your ideal first date? ${questions.question2}
-    - How would we spend our perfect Saturday together? ${questions.question3}
-    - If you could describe our vibe, what would it be? ${questions.question4}
-    - What is your go-to strategy for cheering someone up after a long day? ${questions.question5}
-    - If we were starring in a movie together, what genre would it be and why? ${questions.question6}
+    - Like About me: ${questions.question1}
+    - Ideal first date: ${questions.question2}
+    - Perfect Saturday: ${questions.question3}
+    - Describe our vibe: ${questions.question4}
+    - Cheer up strategy: ${questions.question5}
+    - Movie genre: ${questions.question6}
 
     *Background Check Responses:*
-    - Have you ever been convicted of a crime? ${questions.hrq1}
-    - Do you use drugs recreationally outside of Weed, Tobacco, and vaping? ${questions.hrq2}
-    - Do you currently have hoes? ${questions.hrq3}
-    - Are there any long-standing situationships we need to be aware of? ${questions.hrq4}
-    - Are there kids involved? If so, what do they enjoy doing? ${questions.hrq5}
-    - Are you employed? If so, how long? ${questions.hrq6}
-    - How is your relationship with your family? ${questions.hrq7}
-    - Are you looking for a long-term relationship or a fling? ${questions.hrq8}
-    - How are your feelings about guy/girl relationships? ${questions.hrq9}
+    - Criminal conviction: ${questions.hrq1}
+    - Recreational drugs: ${questions.hrq2}
+    - Current partners: ${questions.hrq3}
+    - Situationships: ${questions.hrq4}
+    - Kids involved: ${questions.hrq5}
+    - Employment status: ${questions.hrq6}
+    - Family relationships: ${questions.hrq7}
+    - Long-term relationship or fling: ${questions.hrq8}
+    - Feelings about guy/girl relationships: ${questions.hrq9}
   `;
+
 
   // Send the formatted message to Pushover
   sendPushoverNotification(message);
